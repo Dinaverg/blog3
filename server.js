4c4732be9c32801df1410aaf00dedb57d5322b44
Darryl Williams <kirbydinaverg@gmail.com>
2019-01-18 22:21:40 -0600

    installing dependacies

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-require('express')
+let express = require('express')
+let uuid = require('uuid')
+let morgan = require('morgan')
+let router = express.Router()
+let bodyParser = require('body-parser')
+let jsonParser = bodyParser.json()
+let app = express()
+
+app.use(morgan('common'))
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
