7a47e68f00a469107626a7030289503a86a2760c
Darryl Williams <kirbydinaverg@gmail.com>
2019-01-23 22:41:33 -0600

    get test

--- test/test-blog.js
+++ test/test-blog.js
@@ -6,3 +6,29 @@ let {app, runServer, closeServer} = require("../server");
 const expect = chai.expect;
 
 chai.use(chaiHttp);
+
+describe("Blog Posts", function() {
+
+    before(function() {
+        return runServer()
+    })
+
+    after(function() {
+        return closeServer()
+    })
+
+    it("should list blog posts on GET", function() {
+        return chai.request(app).get('/blog-posts')
+        .then(function(res) {
+            expect(res).to.have.status(200)
+            expect(res).to.be.json
+            expect(res.body).to.be.an("array")
+            expect(res.body.length).to.be.at.least(1)
+            let expectedKeys = ["id", "title", "content", "author", "publishDate"]
+            res.body.forEach(function(post) {
+                expect(post).to.be.an("object")
+                expect(post).to.include.keys(expectedKeys)
+            })
+        })
+    })
+})
